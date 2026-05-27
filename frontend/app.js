7d602003acfb13c88f64f8e3751b6f687630b9af
Lucas-Sites <lucas.garcia.santana.github.sites@gmail.com>
2026-05-27 19:45:50 -0300

    Código 3

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3001');
+const socket = io('https://morsecodetransmitterserver.onrender.com/');
 
 const mainMenu = document.getElementById('main-menu');
 const joinForm = document.getElementById('join-form');
